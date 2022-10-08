5bc0363fa19cf6084387c7cf02a51f41ac0bcd16
Caio César Leal <caioleal017@gmail.com>
2022-10-08 19:22:45 -0300

    Add files via upload

---
+++ calculadora-script.js
@@ -0,0 +1,157 @@
+let x = 0;
+let y = "";
+let operador = "";
+let result = 0;
+let contNum = 0;
+let contOp = 0;
+let texto = "";
+
+function addNum(numero) {
+	texto = document.getElementById("campo").innerHTML;
+	
+	if(texto.length < 15) {
+		if(contNum == 0) {
+    			if(contOp == 0) {
+    				document.getElementById("campo").innerHTML = numero;
+					texto = document.getElementById("campo").innerHTML;
+        			x = Number(texto.replace(",","."));
+        			contNum = 1;
+					operador = "";
+        		} else {
+        			document.getElementById("campo").innerHTML = numero;
+					texto = document.getElementById("campo").innerHTML;
+        			y = Number(texto.replace(",","."));
+            		contNum = 1;
+        		}
+		}else {
+			if(contOp == 0) {
+				if(texto == "0") {
+					document.getElementById("campo").innerHTML = numero;
+					texto = document.getElementById("campo").innerHTML;
+					x = Number(texto.replace(",","."));
+				} else {
+					document.getElementById("campo").innerHTML += numero;
+					texto = document.getElementById("campo").innerHTML;
+					x = Number(texto.replace(",","."));
+				}
+				
+			} else {
+				if(texto == "0") {
+					document.getElementById("campo").innerHTML = numero;
+					texto = document.getElementById("campo").innerHTML;
+					y = Number(texto.replace(",","."));
+				} else {
+					document.getElementById("campo").innerHTML += numero;
+					texto = document.getElementById("campo").innerHTML;
+					y = Number(texto.replace(",","."));
+				}
+			}
+		}   
+	} else {
+		alert("Não é possível inserir mais de 15 digitos.");
+	}
+}
+
+function addVirgula() {
+	texto = document.getElementById("campo").innerHTML;
+
+	if(!texto.includes(",")) {
+		if(contNum == 0) {
+			document.getElementById("campo").innerHTML = "0,";
+		} else {
+			document.getElementById("campo").innerHTML += ",";
+		}
+		
+		contNum = 1;
+	}
+}
+
+function addOperador(op) {
+	operador = op;
+	contOp = 1;
+	contNum = 0;
+}
+
+function calcResult() {
+	if(y !== "") {
+		switch (operador) {
+			case "+":
+				result = x + y;
+				break;
+			case "-":
+				result = x - y;
+				break;
+			case "*":
+				result = x * y;
+				break;
+			case "/":
+				if(y == 0) {
+					alert("Não é possível dividir por zero.");
+				} else {
+					result = x / y;
+				}
+				break;
+		}
+		
+		texto = result + "";
+		document.getElementById("campo").innerHTML = texto.replace(".",",");
+		x = result;
+		contNum = 0;
+		contOp = 0;
+		
+	} else {
+		y = x;
+		calcResult();
+	}
+    
+}
+
+function apagarDigito() {
+	texto = document.getElementById("campo").innerHTML;
+	
+	if(contOp == 0) {
+		if(texto.length == 1) {
+			document.getElementById("campo").innerHTML = 0;
+			texto = document.getElementById("campo").innerHTML;
+			x = Number(texto.replace(",","."));
+			contNum = 0;
+		} else {
+			document.getElementById("campo").innerHTML = texto.slice(0, (texto.length - 1));
+			texto = document.getElementById("campo").innerHTML;
+			x = Number(texto.replace(",","."));
+		}
+	} else {
+		if(texto.length == 1) {
+			document.getElementById("campo").innerHTML = 0;
+			texto = document.getElementById("campo").innerHTML;
+			y = Number(texto.replace(",","."));
+			contNum = 0;
+		} else {
+			document.getElementById("campo").innerHTML = texto.slice(0, (texto.length - 1));
+			texto = document.getElementById("campo").innerHTML;
+			y = Number(texto.replace(",","."));
+		}
+	}
+}
+
+function limparTudo() {
+	document.getElementById("campo").innerHTML = 0;
+	x = 0;
+	y = "";
+	operador = "";
+	result = 0;
+	contNum = 0;
+	contOp = 0;
+}
+
+function limparCampo() {
+	if(contOp == 0) {
+		document.getElementById("campo").innerHTML = 0;
+		x = 0;
+		contNum = 0;
+	} else {
+		document.getElementById("campo").innerHTML = 0;
+		y = 0;
+		contNum = 0;
+	}
+}
